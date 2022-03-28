1cb625297ea168747f39300097258fc61ecc2c42
Danila Kalinin <70843634+danilakalinin@users.noreply.github.com>
2022-03-28 22:36:23 +0400

    disabled button

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,8 +1,10 @@
 import { observer } from 'mobx-react-lite';
 import React, { useContext, useState } from 'react';
+import { Pressable, Text, TouchableOpacity} from "react-native";
 import { View, TextInput, StyleSheet, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { authStoreContext, useAuthStore } from '../store/store';
+
 
 const AuthForm = observer(() => {
   const [username, setUsername] = useState('59999998022');
@@ -12,6 +14,7 @@ const AuthForm = observer(() => {
 
   return (
     <SafeAreaView style={styles.container}>
+      
       <TextInput onChangeText={setUsername} value={username} placeholder="Логин" style={{ padding: 10 }} />
       <TextInput
         onChangeText={setPassword}
@@ -20,7 +23,7 @@ const AuthForm = observer(() => {
         secureTextEntry
         style={{ padding: 10 }}
       />
-      <Button title="Войти" onPress={() => login(username, password)} />
+      <Button disabled={username.length == 0 || password.length == 0} title="Войти" onPress={() => login(username, password)} />
     </SafeAreaView>
   );
 });
